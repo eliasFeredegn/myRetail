cb5ca529019f4d0ac6309e969f6315a59a5cc234
eferedeg <elias.feredegn@optum.com>
2020-02-22 19:43:29 -0600

    Recommit controller

--- src/main/java/com/myretail/myRetail/controller/MyRetailController.java
+++ src/main/java/com/myretail/myRetail/controller/MyRetailController.java
@@ -1,4 +1,30 @@
-package com.myretail.myRetail.controller;
+package com.myretail.myRetail.Controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.myretail.myRetail.Model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@CrossOrigin(origins = "*")
+@RestController
 public class MyRetailController {
+
+    @Autowired
+    Product product;
+
+    private static final Logger logger = LogManager.getLogger(MyRetailController.class);
+
+    @GetMapping("/products")
+    public ResponseEntity<Product> getproducts(@RequestParam("productId") int productId) throws Exception{
+        logger.info("MyRetailController.java --> getProducts/{id}() :: START ");
+        ObjectMapper objectMapper = new ObjectMapper();
+        logger.info("BenefitApiController.java --> getBenefitResponse() :: END "
+                + objectMapper.writeValueAsString(product));
+        return new ResponseEntity<Product>(product, HttpStatus.OK);
+    }
 }
